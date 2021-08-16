dc27cd5c3dbd2b5e0d97ab609ca83e2e5598acb8
Ariel Davis <adavis@grammatech.com>
2021-08-16 16:04:43 -0500

    fixed formatting issues

--- cpp/include/gtirb_functions/gtirb_functions.hpp
+++ cpp/include/gtirb_functions/gtirb_functions.hpp
@@ -1,102 +1,86 @@
-#include <Module.hpp>
-#include <Context.hpp>
 #include <AuxDataSchema.hpp>
+#include <Context.hpp>
+#include <Module.hpp>
 #include <optional>
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 #ifndef GTIRB_FN_H
 #define GTIRB_FN_H
 
+namespace gtirb {
 
-namespace gtirb{
+class Function {
+  /// A Function is a collection of code blocks, with information
+  /// about what the function should be named and which entry and
+  /// exit blocks it has.
 
-    class Function {
-        /// A Function is a collection of code blocks, with information
-        /// about what the function should be named and which entry and 
-        /// exit blocks it has.
+  using CodeBlockSet = std::unordered_set<CodeBlock*>;
+  using SymbolSet = Module::SymbolSet;
 
-        using CodeBlockSet  = std::unordered_set<CodeBlock *>;
-        using SymbolSet = Module::SymbolSet;
+  UUID uuid;
+  std::optional<CodeBlockSet> entry_blocks = std::nullopt;
+  std::optional<CodeBlockSet> blocks = std::nullopt;
+  std::optional<SymbolSet> name_symbols = std::nullopt;
+  std::optional<CodeBlockSet> exit_blocks = std::nullopt;
+  std::string long_name;
+  void set_name(void);
 
-        UUID uuid;
-        std::optional<CodeBlockSet> entry_blocks = std::nullopt;
-        std::optional<CodeBlockSet> blocks = std::nullopt;
-        std::optional<SymbolSet> name_symbols = std::nullopt;
-        std::optional<CodeBlockSet> exit_blocks = std::nullopt;
-        std::string long_name;
-        void set_name(void);
+public:
+  Function(UUID Uuid, std::optional<CodeBlockSet> Entries,
+           std::optional<CodeBlockSet> Blocks,
+           std::optional<SymbolSet> NameSymbols,
+           std::optional<CodeBlockSet> ExitBlocks)
+      : uuid(Uuid), entry_blocks(Entries), name_symbols(NameSymbols),
+        exit_blocks(ExitBlocks){set_name()};
 
-        public:
-        Function(UUID Uuid, 
-                 std::optional<CodeBlockSet> Entries, 
-                 std::optional<CodeBlockSet> Blocks,
-                  std::optional<SymbolSet> NameSymbols,
-                  std::optional<CodeBlockSet> ExitBlocks
-                  ): 
-                  uuid(Uuid), 
-                  entry_blocks(Entries), 
-                  name_symbols(NameSymbols),
-                  exit_blocks(ExitBlocks) {set_name()};
+  Function(UUID Uuid, CodeBlockSet Entries, CodeBlockSet Blocks,
+           SymbolSet NameSymbols)
+      : uuid(Uuid), entry_blocks(make_optional(Entries)),
+        blocks(make_optional(Blocks)), name_symbols(make_optional(NameSymbols)),
+        exit_blocks(std::nullopt){set_name()};
 
+  Function(UUID Uuid) : uuid(Uuid){set_name()};
 
-        Function( UUID Uuid, 
-                  CodeBlockSet Entries, 
-                  CodeBlockSet Blocks, 
-                  SymbolSet NameSymbols
-                  ):
-                 uuid(Uuid), 
-                 entry_blocks(make_optional(Entries)),
-                 blocks(make_optional(Blocks)),
-                 name_symbols(make_optional(NameSymbols)),
-                 exit_blocks(std::nullopt){set_name()};
+  std::optional<CodeBlockSet>& get_entry_blocks() { return entry_blocks; }
+  const std::optional<CodeBlockSet>& get_entry_blocks() { return entry_blocks; }
 
-        Function(UUID Uuid):
-            uuid(Uuid) {set_name()};
-        
-        std::optional<CodeBlockSet> & get_entry_blocks(){ return entry_blocks; }
-        const std::optional<CodeBlockSet> & get_entry_blocks() { return entry_blocks; }
+  std::optional<CodeBlockSet>& get_exit_blocks() { return exit_blocks; }
+  const std::optional<CodeBlockSet>& get_exit_blocks() { return exit_blocks; }
 
-        std::optional<CodeBlockSet> & get_exit_blocks(){ return exit_blocks; }
-        const std::optional<CodeBlockSet> & get_exit_blocks(){ return exit_blocks; }
-        
-        std::optional<CodeBlockSet> & get_all_blocks(){ return blocks; }
-        const std::optional<CodeBlockSet> & get_all_blocks(){ return blocks; }
-        
-        SymbolSet & get_name_symbols(){ return name_symbols }
-        const SymbolSet & get_name_symbols() { return name_symbols; }
-        
-        
-        UUID get_uuid() {return uuid;}
-        const UUID get_uuid() {return uuid;}
-        UUID & get_uuid() {return uuid;}
-        const UUID & get_uuid() { return uuid; }
+  std::optional<CodeBlockSet>& get_all_blocks() { return blocks; }
+  const std::optional<CodeBlockSet>& get_all_blocks() { return blocks; }
 
-        
-        std::vector<string&> get_names(){
-            std::vector<string&> arr{};
-            if (name_symbols){
-                for (const auto & name_symbol: *name_symbols){
-                    arr.insert(name_symbol->second);
-                }
-            }
-            return arr;
-        }
+  SymbolSet& get_name_symbols() { return name_symbols }
+  const SymbolSet& get_name_symbols() { return name_symbols; }
 
-        /// \brief Get the name of this function, as a string
+  UUID get_uuid() { return uuid; }
+  const UUID get_uuid() { return uuid; }
+  UUID& get_uuid() { return uuid; }
+  const UUID& get_uuid() { return uuid; }
 
-        std::string get_name(){
-        }
-
-        /// \brief Create all the functions present in a \ref Module
-        ///
-        /// \param C The current GTIRB \ref Context
-        /// \param mod 
-
-        /// \return an vector containing the Functions in this module (possibly empty)
-        static vector<Function> build_functions(const Context & C, const Module & mod);
-    
+  std::vector<string&> get_names() {
+    std::vector<string&> arr{};
+    if (name_symbols) {
+      for (const auto& name_symbol : *name_symbols) {
+        arr.insert(name_symbol->second);
+      }
     }
+    return arr;
+  }
 
+  /// \brief Get the name of this function, as a string
+
+  std::string get_name() {}
+
+  /// \brief Create all the functions present in a \ref Module
+  ///
+  /// \param C The current GTIRB \ref Context
+  /// \param mod
+
+  /// \return an vector containing the Functions in this module (possibly empty)
+  static vector<Function> build_functions(const Context& C, const Module& mod);
 }
+
+} // namespace gtirb
 #endif
--- cpp/src/gtirb_functions.cpp
+++ cpp/src/gtirb_functions.cpp
@@ -1,85 +1,78 @@
 #include "gtirb_functions.hpp"
 
-  namespace gtirb{
-        
+namespace gtirb {
 
-        void Function::set_name(void) {
-            if (!this->name_symbols){
-                this->long_name = "<unknown>";
-                return;
-            }
-            auto nsyms = *(this->name_symbols);
-            int n_names = std::length(nsyms);
-            switch (n_names){
-                case 0:
-                    this->long_name = "<unknown>";
-                case 1:
-                    this->long_name = (*(begin(nsyms)))->getName();
-                default:
-                for (auto sym = begin(nsyms);
-                         sym != end(nsyms); 
-                         sym ++ ) {
-                    if (sym == begin(nsyms)) {
-                    this->long_name = (*(sym))->getName();
-                    this->long_name += "(a.k.a ";
-                    }
-                    else
-                    {
-                        this->long_name += (*sym)->getName();
-                        if (sym + 1 < end(nsyms)){
-                            name += ", ";
-                        }
-                    }
-                    this->long_name += ')';
-                }
-            }
-        }
-        
-        static std::vector<Function> Function::build_functions
-        (const Context & C, const Module & mod) const
-        {
-            //build table of symbols by referent.UUID
-            auto symbols = std::unordered_map<UUID, Module::SymbolSet>{};
-            for (const auto & s : mod.symbols()){
-                Node * ref = s.getReferent();
-                if (ref != nullptr){
-                    auto uuid = ref->getUUID();
-                    if (!symbols.contains(uuid)){
-                        symbols[uuid] = Module::SymbolSet{};
-                    }
-                    symbols[uuid].insert(s);
-                }
-            }
-
-            // Load AuxData re: functions
-            schema::FunctionEntries::Type function_entries = 
-                mod.getAuxData<schema::FunctionEntries>();
-
-
-            schema::FunctionBlocks::Type all_fn_blocks = 
-                mod.getAuxData<schema::FunctionBlocks>();
-
-            std::vector<Function> functions();
-            
-            for (const auto & fn_entry: function_entries){
-                Module::SymbolSet syms();
-                auto id = fn_entry.first;
-                auto fn_blocks_uuid = all_fn_blocks[id];
-                auto entry_blocks = Function::CodeBlockSet () ;
-                auto fn_blocks = Function::CodeBlockSet();
-                for(const auto & id: fn_entry.second){
-                    CodeBlock * block = cast<CodeBlock, Node>(Node::getByUUID(C, id));
-                    entry_blocks.insert(block);
-                }
-                for (const auto& b : fn_blocks_uuid){
-                    for (const auto &  s: symbols[b]){
-                        syms.insert(s);
-                    }
-                    CodeBlock * block = cast<CodeBlock, Node>(Node::getByUUID(C, b));
-                    fn_blocks.insert(block);
-                }
-                functions.emplace_back(fn_entry.first, entry_blocks, fn_blocks, syms));
-            }
-            return functions;
-        }
+void Function::set_name(void) {
+  if (!this->name_symbols) {
+    this->long_name = "<unknown>";
+    return;
   }
+  auto nsyms = *(this->name_symbols);
+  int n_names = std::length(nsyms);
+  switch (n_names) {
+  case 0:
+    this->long_name = "<unknown>";
+  case 1:
+    this->long_name = (*(begin(nsyms)))->getName();
+  default:
+    for (auto sym = begin(nsyms); sym != end(nsyms); sym++) {
+      if (sym == begin(nsyms)) {
+        this->long_name = (*(sym))->getName();
+        this->long_name += "(a.k.a ";
+      } else {
+        this->long_name += (*sym)->getName();
+        if (sym + 1 < end(nsyms)) {
+          name += ", ";
+        }
+      }
+      this->long_name += ')';
+    }
+  }
+}
+
+static std::vector<Function>
+Function::build_functions(const Context& C, const Module& mod) const {
+  // build table of symbols by referent.UUID
+  auto symbols = std::unordered_map<UUID, Module::SymbolSet>{};
+  for (const auto& s : mod.symbols()) {
+    Node* ref = s.getReferent();
+    if (ref != nullptr) {
+      auto uuid = ref->getUUID();
+      if (!symbols.contains(uuid)) {
+        symbols[uuid] = Module::SymbolSet{};
+      }
+      symbols[uuid].insert(s);
+    }
+  }
+
+  // Load AuxData re: functions
+  schema::FunctionEntries::Type function_entries =
+      mod.getAuxData<schema::FunctionEntries>();
+
+  schema::FunctionBlocks::Type all_fn_blocks =
+      mod.getAuxData<schema::FunctionBlocks>();
+
+  std::vector<Function> functions();
+
+  for (const auto& fn_entry : function_entries) {
+    Module::SymbolSet syms();
+    auto id = fn_entry.first;
+    auto fn_blocks_uuid = all_fn_blocks[id];
+    auto entry_blocks = Function::CodeBlockSet();
+    auto fn_blocks = Function::CodeBlockSet();
+    for (const auto& id : fn_entry.second) {
+      CodeBlock* block = cast<CodeBlock, Node>(Node::getByUUID(C, id));
+      entry_blocks.insert(block);
+    }
+    for (const auto& b : fn_blocks_uuid) {
+      for (const auto& s : symbols[b]) {
+        syms.insert(s);
+      }
+      CodeBlock* block = cast<CodeBlock, Node>(Node::getByUUID(C, b));
+      fn_blocks.insert(block);
+    }
+                functions.emplace_back(fn_entry.first, entry_blocks, fn_blocks, syms));
+  }
+  return functions;
+}
+} // namespace gtirb
