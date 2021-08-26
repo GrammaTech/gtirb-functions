398c0fc37f22e9c772e220202828ea4fb5fbb2ad
Ariel Davis <adavis@grammatech.com>
2021-08-26 11:53:14 -0500

    more cpack fixes

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -2,16 +2,19 @@ set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
 
+set(LIBGTIRB_MIN_VERSION 1.10.3)
+
 if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib")
   set(CPACK_PACKAGE_NAME "libgtirb-functions")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions")
   set(CPACK_COMPONENTS_ALL library)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb (>=1.10.0)")
-  set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb.so")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb (>=${LIBGTIRB_MIN_VERSION})")
+  # set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb.so")
 
   # do not use a sepate package for debug info in case of RPM
   set(CPACK_RPM_DEBUGINFO_PACKAGE ON)
   set(CPACK_RPM_DEBUGINFO_FILE_NAME "libgtirb-functions-dbg.rpm")
+
 elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib-dbg")
   # this debug info pacakge applies only to Debian generator
   set(CPACK_PACKAGE_NAME "libgtirb-functions-dbg")
@@ -25,7 +28,8 @@ elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dev")
   set(CPACK_COMPONENTS_ALL headers cmake_config cmake_target)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libgtirb-dev, libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
+      "libgtirb-dev (>=${LIBGTIRB_MIN_VERSION}), libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
+  message(CPACK_DEBIAN_PACKAGE_DEPENDS=${CPACK_DEBIAN_PACKAGE_DEPENDS})
   set(CPACK_RPM_PACKAGE_REQUIRES
       "libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}")
 
@@ -34,10 +38,10 @@ elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "driver")
   set(CPACK_PACKAGE_FILE_NAME "gtirb-functions-driver")
   set(CPACK_COMPONENTS_ALL driver)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libgtirb (>=1.10.0), libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})"
+      "libgtirb (>=${LIBGTIRB_MIN_VERSION}), libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})"
   )
   set(CPACK_RPM_PACKAGE_REQUIRES
-      "libstdc++.so.6, libc.so.6, libgcc_s.so.1, libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}"
+      "libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}"
   )
 
   # do not use a sepate package for debug info in case of RPM
