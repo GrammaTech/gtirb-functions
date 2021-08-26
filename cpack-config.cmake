bf3ff49071d12262eb559b4883fe5a82984256c9
Ariel Davis <adavis@grammatech.com>
2021-08-26 10:41:32 -0500

    fixed bugs with cpack config

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -2,48 +2,52 @@ set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
 
-if("${CPACK_HELLO_PACKAGE}" STREQUAL "lib")
-  set(CPACK_PACKAGE_NAME "libhello")
-  set(CPACK_PACKAGE_FILE_NAME "libhello")
+if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib")
+  set(CPACK_PACKAGE_NAME "libgtirb-functions")
+  set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions")
   set(CPACK_COMPONENTS_ALL library)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6, libc6, libgcc1")
-  set(CPACK_RPM_PACKAGE_REQUIRES "libstdc++.so.6, libc.so.6, libgcc_s.so.1")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb (>=1.10.0)")
+  set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb.so")
 
   # do not use a sepate package for debug info in case of RPM
   set(CPACK_RPM_DEBUGINFO_PACKAGE ON)
-  set(CPACK_RPM_DEBUGINFO_FILE_NAME "libhello-dbg.rpm")
-elseif("${CPACK_HELLO_PACKAGE}" STREQUAL "lib-dbg")
+  set(CPACK_RPM_DEBUGINFO_FILE_NAME "libgtirb-functions-dbg.rpm")
+elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib-dbg")
   # this debug info pacakge applies only to Debian generator
-  set(CPACK_PACKAGE_NAME "libhello-dbg")
-  set(CPACK_PACKAGE_FILE_NAME "libhello-dbg")
-  set(CPACK_COMPONENTS_ALL hello-debug-file)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libhello (=${CPACK_HELLO_VERSION})")
-elseif("${CPACK_HELLO_PACKAGE}" STREQUAL "dev")
-  set(CPACK_PACKAGE_NAME "libhello-dev")
-  set(CPACK_PACKAGE_FILE_NAME "libhello-dev")
+  set(CPACK_PACKAGE_NAME "libgtirb-functions-dbg")
+  set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dbg")
+  set(CPACK_COMPONENTS_ALL GTIRB_FUNCTIONS-debug-file)
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS
+      "libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
+
+elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
+  set(CPACK_PACKAGE_NAME "libgtirb-functions-dev")
+  set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dev")
   set(CPACK_COMPONENTS_ALL headers cmake_config cmake_target)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libhello (=${CPACK_HELLO_VERSION})")
+      "libgtirb-dev, libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
   set(CPACK_RPM_PACKAGE_REQUIRES
-      "libstdc++.so.6, libc.so.6, libgcc_s.so.1, libhello = ${CPACK_HELLO_VERSION}"
-  )
-elseif("${CPACK_HELLO_PACKAGE}" STREQUAL "driver")
-  set(CPACK_PACKAGE_NAME "hello-driver")
-  set(CPACK_PACKAGE_FILE_NAME "hello-driver")
+      "libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}")
+
+elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "driver")
+  set(CPACK_PACKAGE_NAME "gtirb-functions-driver")
+  set(CPACK_PACKAGE_FILE_NAME "gtirb-functions-driver")
   set(CPACK_COMPONENTS_ALL driver)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libstdc++6, libc6, libgcc1, libhello (=${CPACK_HELLO_VERSION})")
+      "libstdc++6, libc6, libgcc1, libgtirb (>=1.10.0), libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})"
+  )
   set(CPACK_RPM_PACKAGE_REQUIRES
-      "libstdc++.so.6, libc.so.6, libgcc_s.so.1, libhello = ${CPACK_HELLO_VERSION}"
+      "libstdc++.so.6, libc.so.6, libgcc_s.so.1, libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}"
   )
 
   # do not use a sepate package for debug info in case of RPM
   set(CPACK_RPM_DEBUGINFO_PACKAGE ON)
-  set(CPACK_RPM_DEBUGINFO_FILE_NAME "hello-driver-dbg.rpm")
-elseif("${CPACK_HELLO_PACKAGE}" STREQUAL "driver-dbg")
+  set(CPACK_RPM_DEBUGINFO_FILE_NAME "gtirb-functions-driver-dbg.rpm")
+elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "driver-dbg")
   # this debug info pacakge applies only to Debian generator
-  set(CPACK_PACKAGE_NAME "hello-driver-dbg")
-  set(CPACK_PACKAGE_FILE_NAME "hello-driver-dbg")
-  set(CPACK_COMPONENTS_ALL hello-driver-debug-file)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "hello-driver (=${CPACK_HELLO_VERSION})")
+  set(CPACK_PACKAGE_NAME "gtirb-functions-driver-dbg")
+  set(CPACK_PACKAGE_FILE_NAME "gtirb-functions-driver-dbg")
+  set(CPACK_COMPONENTS_ALL gtirb-functions-driver-debug-file)
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS
+      "gtirb-functions-driver (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
 endif()
