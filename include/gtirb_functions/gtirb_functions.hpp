acb64a07eb12680b979309001a960f3de34dd08c
Ariel Davis <adavis@grammatech.com>
2021-09-09 14:45:17 -0500

    removed mandatory const from api

--- include/gtirb_functions/gtirb_functions.hpp
+++ include/gtirb_functions/gtirb_functions.hpp
@@ -37,8 +37,8 @@ namespace gtirb {
 
 class Function {
 
-  using CodeBlockSet = std::unordered_set<const CodeBlock*>;
-  using SymbolSet = std::unordered_set<const Symbol*>;
+  using CodeBlockSet = std::unordered_set<CodeBlock*>;
+  using SymbolSet = std::unordered_set<Symbol*>;
 
   UUID Uuid;
 
@@ -48,7 +48,7 @@ class Function {
 
   SymbolSet NameSymbols;
 
-  const Symbol* CanonName;
+  Symbol* CanonName;
   std::string LongName;
 
   // helper functions
@@ -79,62 +79,100 @@ public:
 
   /// \section Iterators
 
-  /// \brief Const iterator over code blocks, in an arbitrary order
-  using code_block_iterator = CodeBlockSet::const_iterator;
+  /// \brief Iterators over code blocks, in an arbitrary order
+  using code_block_iterator = CodeBlockSet::iterator;
+  using const_code_block_iterator = CodeBlockSet::const_iterator;
 
-  /// \brief Const range of code blocks
+  /// \brief Const ranges of code blocks
   using code_block_range = ::boost::iterator_range<code_block_iterator>;
+  using const_code_block_range =
+      ::boost::iterator_range<const_code_block_iterator>;
 
   /// \brief Return an iterator to the first entry block
   code_block_iterator entry_blocks_begin() { return EntryBlocks.begin(); }
+  const_code_block_iterator entry_blocks_begin() const {
+    return EntryBlocks.begin();
+  }
+
   /// \brief Return an iterator to the element after the last entry block
   code_block_iterator entry_blocks_end() { return EntryBlocks.end(); }
+  const_code_block_iterator entry_blocks_end() const {
+    return EntryBlocks.end();
+  }
+
   /// \brief Return a range of the entry blocks
   code_block_range entry_blocks() {
+    return {EntryBlocks.begin(), EntryBlocks.end()};
+  }
+  const_code_block_range entry_blocks() const {
     return {EntryBlocks.begin(), EntryBlocks.end()};
   }
 
   /// \brief Return an iterator to the first exit block
   code_block_iterator exit_blocks_begin() { return ExitBlocks.begin(); }
+  const_code_block_iterator exit_blocks_begin() const {
+    return ExitBlocks.begin();
+  }
   /// \brief Return an iterator to the element after the last exit block
   code_block_iterator exit_blocks_end() { return ExitBlocks.end(); }
+  const_code_block_iterator exit_blocks_end() const { return ExitBlocks.end(); }
+
   /// \brief Return a range of the exit blocks
   code_block_range exit_blocks() {
     return {ExitBlocks.begin(), ExitBlocks.end()};
   }
-
+  const_code_block_range exit_blocks() const {
+    return {ExitBlocks.begin(), ExitBlocks.end()};
+  }
   /// \brief Return an iterator to the first code block in the function
   code_block_iterator all_blocks_begin() { return AllBlocks.begin(); }
+  const_code_block_iterator all_blocks_begin() const {
+    return AllBlocks.begin();
+  }
   /// \brief Return an iterator to the element after the last code block
   code_block_iterator all_blocks_end() { return AllBlocks.end(); }
+  const_code_block_iterator all_blocks_end() const { return AllBlocks.end(); }
   /// \brief Return a range of the code blocks in the function
   code_block_range all_blocks() { return {AllBlocks.begin(), AllBlocks.end()}; }
+  const_code_block_range all_blocks() const {
+    return {AllBlocks.begin(), AllBlocks.end()};
+  }
 
-  /// \brief Const iterator over symbols, in arbitrary order
-  using symbol_iterator = SymbolSet::const_iterator;
-  /// \brief Const range over symbols
+  /// \brief Iterators over symbols, in arbitrary order
+  using symbol_iterator = SymbolSet::iterator;
+  using const_symbol_iterator = SymbolSet::const_iterator;
+  /// \brief Ranges over symbols
   using symbol_range = ::boost::iterator_range<symbol_iterator>;
+  using const_symbol_range = ::boost::iterator_range<const_symbol_iterator>;
 
   /// A name symbol is any symbol that refers to an entry block of the function
 
   /// \brief Return an iterator to the first symbol for the function name
   symbol_iterator name_symbols_begin() { return NameSymbols.begin(); }
+  const_symbol_iterator name_symbols_begin() const {
+    return NameSymbols.begin();
+  }
   /// \brief Return an iterator to the element after the last symbol for the
   /// function name
   symbol_iterator name_symbols_end() { return NameSymbols.end(); }
+  const_symbol_iterator name_symbols_end() const { return NameSymbols.end(); }
   /// \brief Return a range over the name symbols of the function
   symbol_range name_symbols() {
     return {NameSymbols.begin(), NameSymbols.end()};
   }
+  const_symbol_range name_symbols() const {
+    return {NameSymbols.begin(), NameSymbols.end()};
+  }
 
   /// \brief Returns the name of the function as recorded in AuxData
-  const Symbol* getName() { return CanonName; }
+  Symbol* getName() { return CanonName; }
+  const Symbol* getName() const { return CanonName; }
 
   /// \brief Returns a pretty concatenation of the names of the functions,
   /// as a string view
   const std::string& getLongName() { return LongName; }
 
-  /// \brief Returns the UUID of the function as a
+  /// \brief Returns the UUID of the function
   const UUID& getUUID() { return Uuid; }
 };
 
