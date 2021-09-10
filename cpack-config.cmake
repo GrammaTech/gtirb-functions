8da70439a3dca5ac8f6ea671228b4acbf9ddc348
Ariel Davis <adavis@grammatech.com>
2021-09-10 11:30:22 -0500

    fixing dependency management with cpack

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -2,12 +2,14 @@ set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
 
+set(CPACK_DEBEIAN_PACKAGE_DEPENDS "libgtirb-${CPACK_GTIRB_VERSION}")
+
 if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib")
   set(CPACK_PACKAGE_NAME "libgtirb-functions")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions")
   set(CPACK_COMPONENTS_ALL library)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb")
-  set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb")
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb (=${CPACK_GTIRB_VERSION})")
+  set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb.so.${CPACK_GTIRB_VERSION}")
 
   # do not use a sepate package for debug info in case of RPM
   set(CPACK_RPM_DEBUGINFO_PACKAGE ON)
@@ -26,9 +28,11 @@ elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dev")
   set(CPACK_COMPONENTS_ALL headers cmake_config cmake_target)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libgtirb-dev, libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
+      "libgtirb-dev (=${CPACK_GTIRB_VERSION}), libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})"
+  )
   message(CPACK_DEBIAN_PACKAGE_DEPENDS=${CPACK_DEBIAN_PACKAGE_DEPENDS})
   set(CPACK_RPM_PACKAGE_REQUIRES
-      "libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}")
+      "libgtirb-dev = ${CPACK_GTIRB_VERSION}, libgtirb-functions = ${CPACK_GTIRB_FUNCTIONS_VERSION}"
+  )
 
 endif()
