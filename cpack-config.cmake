724a220533831dd62f201fd5abd901946b8c265c
Ariel Davis <adavis@grammatech.com>
2021-09-14 16:01:02 -0500

    changing build scripts to only build header library + tests

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -4,26 +4,7 @@ set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)
 
 set(CPACK_DEBEIAN_PACKAGE_DEPENDS "libgtirb-${CPACK_GTIRB_VERSION}")
 
-if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib")
-  set(CPACK_PACKAGE_NAME "libgtirb-functions")
-  set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions")
-  set(CPACK_COMPONENTS_ALL library)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtirb-${CPACK_GTIRB_VERSION}")
-  set(CPACK_RPM_PACKAGE_REQUIRES "libgtirb = ${CPACK_GTIRB_VERSION}")
-
-  # do not use a sepate package for debug info in case of RPM
-  set(CPACK_RPM_DEBUGINFO_PACKAGE ON)
-  set(CPACK_RPM_DEBUGINFO_FILE_NAME "libgtirb-functions-dbg.rpm")
-
-elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "lib-dbg")
-  # this debug info pacakge applies only to Debian generator
-  set(CPACK_PACKAGE_NAME "libgtirb-functions-dbg")
-  set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dbg")
-  set(CPACK_COMPONENTS_ALL gtirb-functions-debug-file)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libgtirb-functions (=${CPACK_GTIRB_FUNCTIONS_VERSION})")
-
-elseif("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
+if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
   set(CPACK_PACKAGE_NAME "libgtirb-functions-dev")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dev")
   set(CPACK_COMPONENTS_ALL headers cmake_config cmake_target)
