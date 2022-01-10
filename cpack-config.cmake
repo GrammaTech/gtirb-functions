da8e47f9babd84cacc68a21df84dad694bbc03f1
Ariel Davis <adavis@grammatech.com>
2022-01-10 15:31:19 -0600

    trying to get gtirb dependency to be correctly named

--- cpack-config.cmake
+++ cpack-config.cmake
@@ -9,7 +9,7 @@ if("${CPACK_GTIRB_FUNCTIONS_PACKAGE}" STREQUAL "dev")
   set(CPACK_PACKAGE_FILE_NAME "libgtirb-functions-dev")
   set(CPACK_COMPONENTS_ALL headers cmake_config cmake_target)
   set(CPACK_DEBIAN_PACKAGE_DEPENDS
-      "libgtirb-dev (>=${CPACK_GTIRB_VERSION}, <${CPACK_NEXT_GTIRB_VERSION})")
+      "libgtirb-dev (=${CPACK_GTIRB_VERSION}-${CPACK_UBUNTU_NAME})")
 
   set(CPACK_RPM_PACKAGE_NAME "libgtirb-functions-devel")
   set(CPACK_RPM_FILE_NAME "${CPACK_RPM_PACKAGE_NAME}.rpm")
